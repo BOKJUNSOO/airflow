0cfaf589c00e9b0317f81f572442214df2cb64a7
brianbok <brianbok97@gmail.com>
2024-12-01 21:30:16 +0900

    dags_conn graph

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -7,7 +7,7 @@ from airflow.operators.empty import EmptyOperator
 with DAG(
     dag_id = "dags_conn_test",
     schedule=None,
-    start_date=pendulum.datetime(2024, 12, 1, tz = "Asiz/Seoul"),
+    start_date=pendulum.datetime(2024, 12, 1, tz = "Asia/Seoul"),
     catchup= False
 ) as dag:
 
