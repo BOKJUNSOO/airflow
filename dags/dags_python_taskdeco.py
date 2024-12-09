39f074cc05a4d1a5d3af411ca8a90e86d148d999
brianbok <brianbok97@gmail.com>
2024-12-09 23:42:13 +0900

    deco with python func and dag

--- dags/dags_python_taskdeco.py
+++ dags/dags_python_taskdeco.py
@@ -2,17 +2,17 @@ from airflow import DAG
 import pendulum
 import datetime
 from airflow.operators.python import PythonOperator
-from airflow.decorators import task
+from airflow.decorators import task, dag
 
-with DAG(
-    dag_id = "dags_python_taskdeco",
-    schedule = "10 0 * * 6#1", 
-    start_date= pendulum.datetime(2024 , 12, 1, tz = "Asia/Seoul"),
-    catchup=False
-) as dag:
-    
-    @task(task_id = "python_task_1")
-    def print_context(some_input):
-        print(some_input)
+@dag(
+    schedule=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["example"],
+)
 
-    python_task = print_context("task_decorator_실행")
+@task(task_id = "python_task_1")
+def print_context(some_input):
+    print(some_input)
+
+python_task = print_context("task_decorator_실행")
