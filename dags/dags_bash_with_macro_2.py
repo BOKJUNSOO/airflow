c0f845c22614b4ab9aa8956b8ca049184c797901
brianbok <brianbok97@gmail.com>
2024-12-16 19:02:31 +0900

    date - > data

--- dags/dags_bash_with_macro_2.py
+++ dags/dags_bash_with_macro_2.py
@@ -14,8 +14,8 @@ with DAG(
     task_1 = BashOperator(
         task_id = "task_1",
         env={
-            'START_DATE':'{{(date_interval_start.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(days=16))|ds}}',
-            'END_DATE':'{{(date_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14))|ds}}'
+            'START_DATE':'{{(data_interval_start.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(days=16))|ds}}',
+            'END_DATE':'{{(data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14))|ds}}'
         },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE" '
         
